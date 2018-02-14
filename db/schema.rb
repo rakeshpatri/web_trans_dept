ce5a8322015b54d9e3a312e2056cc7555fe59cda
rakesh patri <rakeshpatri@gmail.com>
2018-02-14 21:04:50 +0530

    new commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180213153409) do
+ActiveRecord::Schema.define(version: 20180214052600) do
+
+  create_table "applicants", force: :cascade do |t|
+    t.string "name"
+    t.integer "guid"
+    t.boolean "eligible"
+    t.text "criminal_records"
+    t.text "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
